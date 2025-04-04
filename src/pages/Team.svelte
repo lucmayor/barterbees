748899f86ae313e93799fe4221e09d05cf8ee213
lucmayor <lucmayor@gmail.com>
2025-04-04 15:31:29 -0500

    update user page

--- src/pages/Team.svelte
+++ src/pages/Team.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <Page>
-    <p><b>Luc Mayor</b> - <i>developed the "swipe" feature, this site, and base user profile</i></p>
-    <p><b>Ameenah Zeid</b> - <i>co-developed the booking feature, lead testing</i></p>
-    <p><b>Opeyemi Ogundimu</b> - <i>developed the database, login, and base gui elements</i></p>
-    <p><b>Nataniella Ogogo</b> - <i>co-developed the booking feature, search feature base user profile</i></p>
+    <p><b>Luc Mayor</b> - <i>developed the "swipe" feature, this site, original stub database, and base user profile</i></p>
+    <p><b>Ameenah Zeid</b> - <i>co-developed the booking feature coming in a future update, lead testing</i></p>
+    <p><b>Opeyemi Ogundimu</b> - <i>developed the hsql database, login and main user screen, and base gui elements</i></p>
+    <p><b>Nataniella Ogogo</b> - <i>co-developed the booking feature coming in a future update, search feature base user profile</i></p>
     <p><b>Mia Flores</b> - <i>developed the chat feature coming in a future update, assisted in quality assurance</i></p>
 </Page>
 
